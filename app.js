8c8e9956d5af2736cced26bd88b9ff4a40b3ca22
GinoSwift <phillipbank2004@gmail.com>
2023-10-09 17:02:00 +0630

    First Commit

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
-// const correctValue = document.getElementsByClassName("correct_value");
-// const correctValue = document.getElementsByTagName("h2");
-
 const correctValue = document.querySelector("#correct_value");
 const squares = document.querySelectorAll(".square");
 const button = document.querySelector(".btn");
 let isWin = false;
 
-// console.log(correctValue);
-
 const colors = [];
 
-for(let i = 0; i < 9; i++){
-    colors.push(`rgb(${Math.floor(Math.random() * 256)}), ${Math.floor(Math.random() * 256)}), ${Math.floor(Math.random() * 256)})`);
-   
+for (let i = 0; i < 9; i++) {
+    colors.push(`rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`);
 }
 
 const correctColor = colors[Math.floor(Math.random() * 9)];
@@ -20,28 +14,25 @@ correctValue.textContent = correctColor;
 
 squares.forEach((square, i) => {
     square.style.backgroundColor = colors[i];
-    square.setAttribute("color",colors[i]);
-})
-
-// squares[0].style.backgroundColor = colors[0];
+    square.setAttribute("color", colors[i]);
+});
 
 squares.forEach((square) => {
-    square.addEventListener("click", ()=> {
+    square.addEventListener("click", () => {
         const squareColor = square.getAttribute("color");
-        if(!isWin){
-            if(squareColor === correctColor){
+        if (!isWin) {
+            if (squareColor === correctColor) {
                 squares.forEach(squ => {
                     squ.style.backgroundColor = correctColor;
-                })
+                });
                 isWin = true;
-            }
-            else {
+            } else {
                 square.style.backgroundColor = "#0A141D";
             }
         }
-    })  
-})
+    });
+});
 
 button.addEventListener("click", () => {
     location.reload();
-})
+});
